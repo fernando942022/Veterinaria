d54f28c474d80c4dc145c7b9e90a61f68ec4c7d2
FFernandez <fdo.fernandez94@gmail.com>
2022-03-08 07:05:30 -0800

    Agrupacion de la vista Add en la cual ira codigo para realizar consultas cuando provienen desde la lista de mascotas y de la lista de consultas. Antes estaban separadas las vistas que provenia de cada una de las anteriores y existia otra vista llamada newconsult.

---
+++ Vet/Controllers/PayController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vet.Controllers
+{
+    public class PayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Vet/Models/Pay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vet.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vet.Models
+{
+    public class Pay
+    {
+        public int PayId { get; set; }
+
+        /*public int PetId { get; set; }
+        public Pet Pet { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Owner name")]
+        public string OwnerName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Pet name")]
+        public string PetName { get; set; }
+
+        [Required]
+        [Display(Name = "Consult date:   ")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ConsultDate { get; set; }
+
+        [Required]
+        [Display(Name = "Pet weight(Kgs)")]
+        public int PetWeight { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Body temperature")]
+        public string PetTemperature { get; set; }
+
+        [Required]
+        //[StringLength(200)]
+        [AllowHtml]
+        [Display(Name = "Medical pet chart")]
+        public string MedicalChart { get; set; }*/
+    }
+}
