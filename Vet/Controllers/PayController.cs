86c80a1a10bfe4da54f7bfb4b49dc9d248245e87
FFernandez <fdo.fernandez94@gmail.com>
2022-03-09 21:14:16 -0800

    Se genera codigo para ingresar un nuevo pago en la BD Pay.

--- Vet/Controllers/PayController.cs
+++ Vet/Controllers/PayController.cs
@@ -3,14 +3,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vet.Data;
+using Vet.Models;
 
 namespace Vet.Controllers
 {
     public class PayController : Controller
     {
+        StoreContext Dbcon = null;
+
+        public PayController(StoreContext con)
+        {
+            Dbcon = con;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int consultId)
+        {
+            return Json(Dbcon.Pays.Where(p => p.ConsultId == consultId));
+        }
+
+        [HttpPost]
+        public IActionResult Add(Pay model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Dbcon.Pays.Add(model);
+            Dbcon.SaveChanges();
+            return Json(1);
         }
     }
 }
